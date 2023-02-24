76f8d8ee6c7f2ae65040dcb79c27c45721f54d47
Mrs SORADA WRIGHT <93084381+SoradaW@users.noreply.github.com>
2023-02-24 19:27:06 +0000

    Install npm install inquirer@6.5.0

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+// internal modules
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inquirer = require("inquirer");
+
+// external packages
+const inquirer = require("inquirer"); 
 const path = require("path");
 const fs = require("fs");
 
